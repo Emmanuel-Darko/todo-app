4cdff04db2ad9443a5ced8eebbd9116a61829d49
Emmanuel Ofosu. Darko <Emmanuel Darko@maximnyansa.org>
2023-12-01 15:50:55 +0000

    implement vite-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,28 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      workbox: {
+        cleanupOutdatedCaches: true,
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,json,vue,txt,woff2}']
+      },
+      manifest: {
+        name: 'Task Master',
+        short_name: 'TaskMaster',
+        description: 'A task scheduler app',
+        theme_color: '#FF5631',
+        background_color: '#191919',
+        display: 'standalone'
+      }
+    })
   ],
   resolve: {
     alias: {
